67b338b41161826029c18fc05a74f98bfaf66ead
justintjoa <justintoja@gmail.com>
2020-09-30 22:36:20 -0700

    first commit

---
+++ src/main/scala/com/example/JsonFormats.scala
@@ -0,0 +1,18 @@
+package com.example
+
+import com.example.Services.{User, Users}
+import com.example.Services.UserRegistry.ActionPerformed
+
+//#json-formats
+import spray.json.DefaultJsonProtocol
+
+object JsonFormats  {
+  // import the default encoders for primitive types (Int, String, Lists etc)
+  import DefaultJsonProtocol._
+
+  implicit val userJsonFormat = jsonFormat3(User)
+  implicit val usersJsonFormat = jsonFormat1(Users)
+
+  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+}
+//#json-formats
